70d5e335f97900d24dccab90466ef419dda85e9c
matet2001 <matet2001@gmail.com>
2025-08-13 17:26:35 +0200

    Made footer

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,8 @@ export const CONTACT = {
     phoneDisplay: '06 30 536-7500',
     email: 'info@artistfactory.hu',
     address: 'Budapest, XIII. ker. Pozsonyi út 16.',
+    coords: {
+        lat: 47.515181710128985,
+        lng: 19.049547169919382,
+    },
 }
